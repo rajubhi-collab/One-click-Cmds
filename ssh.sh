af186bbff895e64111602264f9e58991adcf701f
rajubhi-collab <rajbhai00797@gmail.com>
2026-03-26 10:23:44 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -44,7 +44,7 @@ sleep 1
 
 # Custom MOTD Install
 echo -e "\033[1;34m▶ Installing Custom MOTD...\033[0m"
-bash <(curl -fsSL https://raw.githubusercontent.com/OfficialCodesHub/One-click-Cmds/refs/heads/main/motd.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/rajubhi-collab/One-click-Cmds/refs/heads/main/motd.sh)
 
 echo -e "\033[1;32m✔ Custom MOTD Installed!\033[0m"
 sleep 1
